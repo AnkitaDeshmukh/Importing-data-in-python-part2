8fa4518da1c4e17560a29dfe16a35b0c37bb66d3
Ankita Deshmukh <42597079+AnkitaDeshmukh@users.noreply.github.com>
2018-09-06 18:28:22 -0700

    Update Plotting your Twitter data.py

--- diving deep into the twitter API/Plotting your Twitter data.py
+++ diving deep into the twitter API/Plotting your Twitter data.py
@@ -22,7 +22,7 @@ sns.set(color_codes=True)
 cd = ['clinton', 'trump', 'sanders', 'cruz']
 
 # Plot histogram
-ax = sns.barplot(cd, [clinton, trump, sanders, cruz)
+ax = sns.barplot(cd, [clinton, trump, sanders, cruz])
 ax.set(ylabel="count")
 plt.show()
 
